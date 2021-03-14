a678f4e792c89322c3d3b951d9c6e96e23207c8e
novikov-M-N <mnovikov37@gmail.com>
2021-03-14 16:31:08 +0700

    lesson2Homework

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import arraylist.MyArrayList;
 import linkedlist.MyLinkedList;
 
 import java.util.ArrayList;
@@ -7,6 +8,8 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+        // Тестирование функционала LinkedList
+
         // Создание пустого линкед листа
         MyLinkedList<String> linkedList = new MyLinkedList<>();
         System.out.println(linkedList + "  - Создание пустого линкед листа");
@@ -77,5 +80,68 @@ public class Main {
         List<String> list = new ArrayList<>(Arrays.asList("11", "12", "13", "14", "15"));
         MyLinkedList<String> linkedListFromCollection = new MyLinkedList<>(list);
         System.out.println(linkedListFromCollection + " - лист, созданный на базе коллекции");
+
+        // Тестирование функционала ArrayList
+
+        // Создаём лист на базе массива
+        MyArrayList<String> arrayList1 = new MyArrayList<>(new String[]{"1-1", "1-2", "1-3", "1-4"});
+        System.out.println(arrayList1 + " - Лист, созданный на базе массива");
+
+        System.out.println(arrayList1.size() + " - Размер листа");
+
+        System.out.println(arrayList1.isEmpty() + " - Лист не пуст");
+
+        System.out.println(arrayList1.containsAll(new ArrayList<>(Arrays.asList(new String[]{"1-2", "1-4"})))
+                + " - лист содержит набор элементов \"1-2\" и \"1-4\"");
+
+        // Cоздаём пустой лист
+        MyArrayList<String> arrayList2 = new MyArrayList<>();
+        System.out.println(arrayList2 + " - Новый пустой лист");
+        System.out.println(arrayList2.isEmpty() + " - Новый лист пуст");
+
+        System.out.println(arrayList1.contains("1-3") + " - Первый лист содержит объект \"1-3\"");
+        System.out.println(arrayList1.contains(10) + " - Первый лист не содержит объект 10");
+
+        for (Object o : arrayList1.toArray()) { System.out.print(o + " "); }
+        System.out.println("- Массив объектов на базе первого листа");
+        for (Object o : arrayList2.toArray()) { System.out.print(o + " "); }
+        System.out.println("- Массив объектов на базе второго листа (пустой)");
+
+        System.out.println(arrayList1.get(2) + " - Элемент с индексом 2");
+
+        System.out.println(arrayList1.set(2, "new") + " - Замена элемента с индексом 2");
+        System.out.println(arrayList1 + " - Лист после замены элемента с индексом 2");
+        System.out.println(arrayList1.set(5, "newnew") + " - Попытка замены элемента с индексом 5 (не сработает)");
+
+        arrayList1.add(2, "insert");
+        System.out.println(arrayList1 + " - Лист после вставки нового элемента на место с индексом 2");
+
+        System.out.println(arrayList1.indexOf("1-4") + " - Индекс объекта \"1-4\"");
+        System.out.println(arrayList1.indexOf("1-5") + " - Попытка получить индекс объекта \"1-5\"");
+
+        arrayList1.add("1-2");
+        System.out.println(arrayList1 + " - Добавляем ещё один объект \"1-2\"");
+        System.out.println(arrayList1.lastIndexOf("1-2") + " - Последний индекс объекта \"1-2\"");
+
+        System.out.println(arrayList1.remove(1) + " - Удаляем элемент с индексом 1");
+        System.out.println(arrayList1 + " - Лист после удаления элемента с индексом 1");
+        System.out.println(arrayList1.remove(5) + " - Попытка удалить элемент с индексом 5");
+
+        System.out.println(arrayList1.remove("1-1") + " - Удаляем элемент \"1-1\"");
+        System.out.println(arrayList1 + " - Лист 1 после удаления элемента \"1-1\"");
+
+        arrayList1.clear();
+        System.out.println(arrayList1 + " - Лист 1 после очистки");
+
+        System.out.println(arrayList1.getCapacity() + " - Ёмкость листа 1");
+        System.out.println(arrayList1.getStorageLimit() + " - Предельная загрузка листа 1");
+
+        // Наглядное отслеживание изменения внутренней структуры листа при добавлении новых элементов
+        for (int i = 0; i <20 ; i++) {
+            arrayList1.add(String.valueOf(i));
+            System.out.println(arrayList1.getCapacity() + " " + arrayList1.getStorageLimit() + " "
+                    + arrayList1.size() + " " + arrayList1);
+        }
+
     }
 }
